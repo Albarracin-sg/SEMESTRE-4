a1dcd67ec700f4287c2944f28a2006770efa9ad8
Albarracin-sg <camilinplin2024@gmail.com>
2025-06-04 10:42:37 -0500

    Add basic arithmetic operations script

---
+++ OPEN SOURCE/CLASE4/operadoresLogicos.py
@@ -0,0 +1,20 @@
+print("\n --------Variables-----------")
+a = float(input("\nDigita un numero: "))
+b = float(input("Digita un segundo numero: "))
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b         
+divisionEntera = a // b 
+modulo = a % b           
+potencia = a ** b        
+
+print("\n --------Resultados-----------")
+print("\nSuma: ", suma)
+print("Resta: ", resta)
+print("Multiplicación: ", multiplicacion)
+print("División exacta: ", division)
+print("División entera: ", divisionEntera)
+print("Módulo: ", modulo)
+print("Potencia: ", potencia)
