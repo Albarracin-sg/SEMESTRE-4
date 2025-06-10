dced0f0f05517db4e4980c7c080e4114f343f85a
Jajavier2404 <javialex2008@gmail.com>
2025-06-09 20:31:36 -0500

    Implementacionde tailwind porque le quedo grande a camilo

--- AMBIENTES Y DESARROLLO SOSTENIBLE/carbon-footprint-app/src/App.jsx
+++ AMBIENTES Y DESARROLLO SOSTENIBLE/carbon-footprint-app/src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1>holaaaa</h1>
+      <h1 className="font-bold text-3xl text-blue-600">holaaaa</h1>
     </>
   )
 }
